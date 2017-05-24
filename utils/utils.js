a9501a0f99e1400893006c8537f6c6b5fe5fc193
Arun <arunkmr492@gmail.com>
2017-05-24 21:02:53 +0530

    date validation fixed

--- utils/utils.js
+++ utils/utils.js
@@ -4,9 +4,9 @@ var chrono = require('chrono-node');
 var sanitizeDate = function(input, callback){
   if(input){
     //var checkDate = chrono.parse(input);
-    if(Object.prototype.toString.call(chrono.parse(input)[0].ref) === '[object Date]'){
+    if(Object.prototype.toString.call(chrono.parse(input)[0].start.date()) === '[object Date]'){
       console.log("date object validity",chrono.parse(input));
-      callback(null, chrono.parse(input)[0].ref);
+      callback(null, chrono.parse(input)[0].start.date());
     }else{
       callback('invalid_date', null);
     }

bcfecd7809bd01442f5dc782cba04daf32e3f3a1
Arun <arunkmr492@gmail.com>
2017-05-24 21:07:12 +0530

    added text

--- modules/actions.js
+++ modules/actions.js
@@ -179,7 +179,7 @@ module.exports = (bot) => {
           callback: (payload, convo) => {
             console.log('button payload', payload);
             const text = payload.postback.payload;
-            convo.say('I need more time to perform the further steps, I am still learning.');
+            convo.say("Sorry I am still learning, my masters haven't taught me how to perform this task yet.");
             convo.end();
             //convo.say(`Great, here's a quick summary`).then(() => editInfo(convo))
           }

55c95b08d099b4681c3dc6f3253b40df8fbfb24a
Arun <arunkmr492@gmail.com>
2017-05-24 19:26:35 +0530

    fuck me

--- utils/utils.js
+++ utils/utils.js
@@ -5,7 +5,7 @@ var sanitizeDate = function(input, callback){
   if(input){
     //var checkDate = chrono.parse(input);
     if(Object.prototype.toString.call(chrono.parse(input)[0].ref) === '[object Date]'){
-      callback(null, checkDate[0].ref);
+      callback(null, chrono.parse(input)[0].ref);
     }else{
       callback('invalid_date', null);
     }

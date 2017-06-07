a66b84abfd8dabda2002467887f53bf0096a90ed
Arun <arunkmr492@gmail.com>
2017-06-07 13:26:41 +0530

    text input disabled

--- modules/actions.js
+++ modules/actions.js
@@ -6,7 +6,7 @@ var Agent = require('./../lib/agent.js');
 var	agent = new Agent();
 var async = require('async');
 
-const disableInput = false;
+const disableInput = true;
 var initiated = false; //Set this to true when the first initiation happens with the bot
 
 module.exports = (bot) => {

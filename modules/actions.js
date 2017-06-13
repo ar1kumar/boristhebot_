149ec35275d0c29e20a7ccceebc55cd06e03d697
Arun <arunkmr492@gmail.com>
2017-06-13 16:29:48 +0530

    adding select time option - debugging

--- modules/actions.js
+++ modules/actions.js
@@ -192,9 +192,9 @@ module.exports = (bot) => {
       ])
   };
 
-  const availableTimes = (convo, courtslist, timings) => {
+  const availableTimes = (convo, courts, timings) => {
     convo.ask((convo)=>{
-      var courtSelected = courtslist[convo.get('court').split("#")[0]];
+      var courtSelected = courts[convo.get('court').split("#")[0]];
       convo.sendGenericTemplate([{
          "title": courtSelected.name,
          "image_url": courtSelected.images[0],
@@ -226,7 +226,7 @@ module.exports = (bot) => {
           console.log('button payload', payload);
           const text = payload.postback.payload;
           convo.set('time', text);
-          convo.say(`Great, here's a quick summary`).then(() => sendSummary(convo, courtslist))
+          convo.say(`Great, here's a quick summary`).then(() => sendSummary(convo, courts))
         }
       }
     ])

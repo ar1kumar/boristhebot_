c965497f911108e115211bf66ff12146b2182e52
Arun <arunkmr492@gmail.com>
2017-06-18 23:31:24 +0530

    use the userid returned from webview - typo fixed

--- modules/actions.js
+++ modules/actions.js
@@ -282,11 +282,11 @@ module.exports = (bot) => {
                  },
                  {
                    "type":"web_url",
-                   "url":"https://sportingbot.forever-beta.com/webview/invite.html?booking_id="+booking._id+"&user_id="+convo.user_id,
+                   "url":"https://sportingbot.forever-beta.com/webview/invite.html?booking_id="+booking._id+"&user_id="+convo.userId,
                    "title":"Invite friends",
                    "webview_height_ratio": "compact",
                    "messenger_extensions": true,
-                   "fallback_url" : "https://sportingbot.forever-beta.com/webview/invite_fallback.html?booking_id="+booking._id+"&user_id="+convo.user_id,
+                   "fallback_url" : "https://sportingbot.forever-beta.com/webview/invite_fallback.html?booking_id="+booking._id+"&user_id="+convo.userId,
                    "webview_share_button" : "hide"
                  }
                ]
